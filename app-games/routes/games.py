a7109b73d9663d86472da2b40f452563c752c067
Vacation Junk Mail <vacationjunkmail@gmail.com>
2019-11-23 08:47:03 -0500

    Added images and removed some

--- app-games/routes/games.py
+++ app-games/routes/games.py
@@ -34,7 +34,7 @@ def show_all_games(console_id):
 	console_results = g.mysql_db.select_params(query,params)
 	query = '''select c.id as console_id,v.id,v.name,v.small_image,v.large_image 
 		   from games.game_console as c inner join games.video_games as v on v.console_id = c.id 
-	 	   where c.id = %s'''
+	 	   where c.id = %s;'''
 	results = g.mysql_db.select_params(query,params)
 	return render_template('games/show_all_games.html', menu = g.menu[1],title = console_results[1][0]['console_name'],shortname = console_results[1][0]['console_shortname'],data = results[1])
 
@@ -50,7 +50,8 @@ def single_game(console_id,game_id):
                    where c.id = %s and v.id = %s;'''
 	results = g.mysql_db.select_params(query,params)
 	params = [game_id]
-	query = '''select c.name from games.characters as c inner join games.video_game_and_characters as v on v.character_id =c.id  where video_game_id = %s;'''
+	query = '''select c.name from games.characters as c inner join games.video_game_and_characters as v on v.character_id =c.id  
+		   where video_game_id = %s order by c.order_num,c.name;'''
 	characters = g.mysql_db.select_params(query,params)
 	return render_template('games/single_game.html',menu = g.menu[1],title = console_results[1][0]['console_name'],shortname = console_results[1][0]['console_shortname'],data = results[1],characters = characters[1])
 
