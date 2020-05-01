d7a72c956433cf93c673c8cabcbc9d538f0bfd4a
Junk Mail <vacationjunkmail@gmail.com>
2020-05-01 19:17:39 -0400

    This was the first attempt at the app factory.  Has been moved to calendar_app

--- cal_app/config.py
+++
@@ -1,8 +0,0 @@
-from os import environ
-
-class Config:
-	SECRET_KEY = environ.get('SECRET_KEY')
-	FLASK_ENV = environ.get('FLASK_ENV')
-
-	STATIC_FOLDER = environ.get('STATIC_FOLDER')
-	TEMPLATES_FOLDER = environ.get('TEMPLATES_FOLDER')
