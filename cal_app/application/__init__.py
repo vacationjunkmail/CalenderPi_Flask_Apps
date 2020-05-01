dd75df145810d930f912d7f5c76e8f79dad811d3
Junk Mail <vacationjunkmail@gmail.com>
2020-05-01 06:49:40 -0400

    Modded file

--- cal_app/application/__init__.py
+++ cal_app/application/__init__.py
@@ -13,7 +13,10 @@ def create_app():
 		from .routes import index_routes
 		from .routes import cal_routes
 		from .routes import camera_routes
+		from .routes import pyscripts_routes
+
 		app.register_blueprint(index_routes.index_bp)
 		app.register_blueprint(cal_routes.cal_bp)
+		app.register_blueprint(pyscripts_routes.script_bp,url_prefix="/pythonscripts/")
 		app.register_blueprint(camera_routes.camera_bp,url_prefix="/camera/")
 	return app
