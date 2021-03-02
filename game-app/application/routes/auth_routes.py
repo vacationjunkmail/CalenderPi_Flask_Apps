b4258ac98b4018780026f84cdf1e2c4d4b3f525f
Junk Mail <vacationjunkmail@gmail.com>
2021-03-02 05:14:58 -0500

    Added code for auth

--- game-app/application/routes/auth_routes.py
+++ game-app/application/routes/auth_routes.py
@@ -5,13 +5,14 @@ from application.sql_queries.sql_statements import app_queries
 from mysql_conn.connect_mysql import get_connection
 import base64
 from pathlib import Path
+import random
 
 auth_bp = Blueprint('auth_bp',__name__,static_folder='static')
 
 @auth_bp.before_request
 def before_request():
 	g.db = get_connection()
-	query = '''select id,console_name,console_shortname,coalesce(twitter,'') as twitter,coalesce(facebook,'') as facebook from auths.auth_console;'''
+	query = '''select id,console_name,console_shortname,coalesce(twitter,'') as twitter,coalesce(facebook,'') as facebook from games.game_console;'''
 	g.menu = g.db.select_no_params(query)
 	g.menu_title = 'Game Consoles'
 
@@ -23,7 +24,14 @@ def after_request(resp):
 @auth_bp.route('/',methods=['GET'])
 @auth_bp.route('/login/',methods=['GET'])
 def home():
-	return render_template('auth/index.html',title='Login',menu=g.menu[1],body='home',menu_title = g.menu_title)
+	item = random.randrange(0,len(g.menu[1]),1)
+	sn = g.menu[1][item]['console_shortname']
+	query = '''select c.id as console_id,v.id,v.name,v.small_image,v.large_image 
+           from games.game_console as c inner join games.video_games as v on v.console_id = c.id 
+           where c.id = %s order by v.name;'''
+	results = g.db.select_params(query,[g.menu[1][item]['id']])
+	game_len = random.randrange(0,len(results[1]),)
+	return render_template('auth/index.html',title='Login',menu=g.menu[1],body='home',menu_title = g.menu_title,data = [results[1][game_len]],shortname=sn)
 
 @auth_bp.route('/logout/',methods=['GET'])
 def two():
