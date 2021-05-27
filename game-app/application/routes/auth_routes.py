9d4a0b93ea009ef7c3588f69e1edbf369e484aa1
Junk Mail <vacationjunkmail@gmail.com>
2021-05-26 20:32:00 -0400

    added new style.  bootstrap4 and fontawesome4

--- game-app/application/routes/auth_routes.py
+++ game-app/application/routes/auth_routes.py
@@ -12,8 +12,8 @@ auth_bp = Blueprint('auth_bp',__name__,static_folder='static')
 @auth_bp.before_request
 def before_request():
 	g.db = get_connection()
-	query = '''select id,console_name,console_shortname,coalesce(twitter,'') as twitter,coalesce(facebook,'') as facebook from games.game_console;'''
-	g.menu = g.db.select_no_params(query)
+	#query = '''select id,console_name,console_shortname,coalesce(twitter,'') as twitter,coalesce(facebook,'') as facebook from games.game_console;'''
+	#g.menu = g.db.select_no_params(query)
 	g.menu_title = 'Game Consoles'
 
 @auth_bp.after_request
@@ -24,14 +24,14 @@ def after_request(resp):
 @auth_bp.route('/',methods=['GET'])
 @auth_bp.route('/login/',methods=['GET'])
 def home():
-	item = random.randrange(0,len(g.menu[1]),1)
-	sn = g.menu[1][item]['console_shortname']
-	query = '''select c.id as console_id,v.id,v.name,v.small_image,v.large_image 
-           from games.game_console as c inner join games.video_games as v on v.console_id = c.id 
-           where c.id = %s order by v.name;'''
-	results = g.db.select_params(query,[g.menu[1][item]['id']])
-	game_len = random.randrange(0,len(results[1]),)
-	return render_template('auth/index.html',title='Login',menu=g.menu[1],body='home',menu_title = g.menu_title,data = [results[1][game_len]],shortname=sn)
+	#item = random.randrange(0,len(g.menu[1]),1)
+	#sn = g.menu[1][item]['console_shortname']
+	#query = '''select c.id as console_id,v.id,v.name,v.small_image,v.large_image 
+    #       from games.game_console as c inner join games.video_games as v on v.console_id = c.id 
+    #       where c.id = %s order by v.name;'''
+	#results = g.db.select_params(query,[g.menu[1][item]['id']])
+	#game_len = random.randrange(0,len(results[1]),)
+	return render_template('auth/index.html',title='Login',body='home',menu_title = g.menu_title)
 
 @auth_bp.route('/logout/',methods=['GET'])
 def two():
